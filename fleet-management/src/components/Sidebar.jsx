c691caa7fd1b024d6a856fb14881e68116193a8a
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2025-12-28 19:57:10 +0530

    admin page created

--- fleet-management/src/components/Sidebar.jsx
+++ fleet-management/src/components/Sidebar.jsx
@@ -3,23 +3,30 @@ import React, { useState } from 'react'
 const Sidebar = () => {
 
     const [formData,setFormData] = useState([]);
-    const handleFleetData = ()=>{
-
+    const [regNo,setRegNo] = useState("");
+    const [driverName,setDriverName]= useState("");  
+    const handleFleetData = (e)=>{
+        e.preventDefault();
+        const fleet = {
+            regNo,
+            driverName,
+            
+        }
     }
 
   return (
     <div className='sidebar'>
        <h2>Add Fleet</h2>
         <form>
-            <input type="text" placeholder='Vehicle Registration number' />
-            <select>
+            <input type="text" placeholder='Vehicle Registration number' value={regNo} onChange={(e)=>setRegNo(e.target.value)} />
+            <select value={value} onChange={(e)=>setCategory(e.target.value)}>
                 <option value="">Select Category</option>
                 <option value="Auto">Auto</option>
                 <option value="Car">Car</option>
                 <option value="Truck">Truck</option>
                 <option value="Bus">Bus</option>
             </select>
-            <input type="text" placeholder='Driver name' />
+            <input type="text" placeholder='Driver name' value={driverName} onChange={(e)=>setDriverName(e.target.value)}/>
             <select>
                 <option value="">Availability Status</option>
                 <option value="Available">Available</option>
