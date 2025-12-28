f3a52d7e4ccb82d7498af3b843fb853aad23627a
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2025-12-28 19:20:36 +0530

    protected route created

--- fleet-management/src/App.jsx
+++ fleet-management/src/App.jsx
@@ -1,9 +1,21 @@
 import Login from "./pages/Login";
-
+import {BrowserRouter, Routes,Route, Navigate} from "react-router-dom";
+import Admin from "./pages/Admin";
+import ProtectedRoute from "./components/ProtectedRoute";
 function App(){
 
   return(
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={"/login"}/>}/>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/admin" element={
+          <ProtectedRoute>
+            <Admin/>
+          </ProtectedRoute>
+        }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- fleet-management/src/pages/Login.jsx
+++ fleet-management/src/pages/Login.jsx
@@ -1,15 +1,19 @@
 import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login(){
 
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
-    const inputRef=useRef();
-    inputRef.current.focus();
+    const navigate = useNavigate();
+    // const inputRef=useRef(null);
+    
 
     const handleLogin = () => {
         if(email === "admin@gmail.com" && password === "admin1234"){
+            localStorage.setItem("isLoggedIn",true);
             alert("Login success");
+            navigate("/admin");
         }
         else{
             alert("Wrong email or password");
@@ -17,7 +21,7 @@ function Login(){
     }
     return(
         <div>
-            <input type="email" placeholder="Enter email" ref={inputRef} onChange={(e)=>setEmail(e.target.value)}/>
+            <input type="email" placeholder="Enter email" onChange={(e)=>setEmail(e.target.value)}/>
             <input type="password" placeholder="Enter password " onChange={(e)=>setPassword(e.target.value)} />
             <button onClick={handleLogin}>Login</button>
         </div>
